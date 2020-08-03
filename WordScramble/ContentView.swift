774d545b5d41ba1b1482aea60052afc80a83a19f
alvarovs89 <67175098+alvarovs89@users.noreply.github.com>
2020-08-03 14:03:17 +1000

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Alvaro Valdes Salazar on 03-08-20.
+//  Copyright © 2020 Alvaro Valdes Salazar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
